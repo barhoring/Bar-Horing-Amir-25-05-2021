9cb69af33969ed8975174793c659f954e063361d
Bar Horing Amir <bar.apple@gmail.com>
2021-05-30 11:26:26 +0300

    skeleton for the app

---
+++ src/components/DayForecast.js
@@ -0,0 +1,15 @@
+
+const DayForecast = () => {
+  return (
+    <div className="day-forecast">
+      <div>
+        Sun
+      </div>
+      <div>
+        38 0 C
+      </div>
+    </div>
+  );
+}
+
+export default DayForecast;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+      <header className="logo-header">
+        <div className="title">
+          Herolo Weather Task
+        </div>
+        <div>
+          Home | Favorites
+        </div>
+      </header>
+  );
+}
+
+export default Header;
