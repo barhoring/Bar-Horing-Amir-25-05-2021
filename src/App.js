fc1edc3fa4233d256fcc88f075dfc77deeae1e31
Bar Horing Amir <bar.apple@gmail.com>
2021-06-01 01:45:51 +0300

    geolocation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { useEffect } from "react";
 import { connect } from 'react-redux';
 import Header from './components/Header';
 import MainPage from "./components/screens/MainPage";
 import FavoritesPage from "./components/screens/FavoritesPage"
-
+import { API_GEOPOSITION_BASE, API_KEY1 } from "./constants";
 import { Router } from "@reach/router";
 
 const mapStateToProps = state => {
@@ -13,7 +14,38 @@ const mapStateToProps = state => {
   };
 };
 
-function App({ isDarkMode }) {
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    handleSelectLocation: (currentLocation) => dispatch({ type: 'SELECT_LOCATION_KEY', payload: { currentLocation }}),
+  }
+};
+
+
+function App({ isDarkMode, handleSelectLocation }) {
+  function success(position) {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+  
+    const uri_current_conditions = `${API_GEOPOSITION_BASE}?apikey=${API_KEY1}&q=${latitude},${longitude}`;
+    fetch(uri_current_conditions)
+    .then(res => res.json()).then(res => {
+      console.log(res);
+      handleSelectLocation(res)
+    }).catch(e => { console.log(e)});
+
+
+    console.log("latitude", latitude);
+    console.log("longitude", longitude);
+  }
+
+  useEffect(()=>{
+    if(!navigator.geolocation) {
+      console.log('Geolocation is not supported by your browser');
+    } else {
+      navigator.geolocation.getCurrentPosition(success, (error) => console.log(error));
+    }
+  }, [])
+
   return (
     <div className={isDarkMode ? "dark-mode" : ""}>
       <Header />
@@ -25,6 +57,4 @@ function App({ isDarkMode }) {
   );
 }
 
-export default connect(mapStateToProps, {})(App);
-
-// export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
