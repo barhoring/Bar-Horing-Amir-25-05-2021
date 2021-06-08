65e849977a70bbd00668f658619cadc341ad5632
Bar Horing Amir <bar.apple@gmail.com>
2021-06-08 18:48:18 +0300

    s

--- src/components/Header/Navigation.js
+++ src/components/Header/Navigation.js
@@ -3,10 +3,9 @@ import { Link } from "@reach/router";
 const Navigation = () => {
   return (
     <div>
-      <Link to="/">Home</Link> |
-      <Link to="/favorites">Favorites</Link>
+      <Link to="./">Home</Link> |<Link to="./favorites">Favorites</Link>
     </div>
   );
-}
+};
 
 export default Navigation;
